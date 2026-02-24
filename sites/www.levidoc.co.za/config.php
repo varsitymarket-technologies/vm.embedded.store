be03cc15f93a3e6fdc52476dc58263a4eb55ad5a
levidoc <levidoc00@gmail.com>
2026-02-24 22:22:11 +0200

    Removed Testing Websites

--- sites/www.levidoc.co.za/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("__SITE_LOGO__","/assets/favicon.png");
-define("__SHOP_INTRO__","URAH's KICKS");
-define("__SHOP_HERO_IMAGE__","https://swestkicks.co.za/cdn/shop/products/nike-dunk-low-gradient-swoosh-gs-140200.jpg?v=1710532895&width=1000");
-define("__SYSTEM_CURRENCY__","R");
