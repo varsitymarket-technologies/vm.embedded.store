7fcbe8e327c264893dc8a9835f49d598df38549b
levidoc <levidoc00@gmail.com>
2026-06-16 23:26:33 +0200

    feat: add store ID retrieval function and update environment configuration

--- t.php
+++ t.php
@@ -1,4 +1,21 @@
 <?php 
-$theme_file = dirname(__FILE__) . "/themes/records.json"; 
-echo file_get_contents($theme_file);
+
+
+@include_once dirname(__FILE__)."/config.php";
+
+
+
+function _get_store_id( $domain , $db_engine = __DB_MODULE__ ){
+    $store_record = $db_engine->query("SELECT * FROM sys_websites WHERE account_index = ? LIMIT 1", [__ACCOUNT_INDEX__]);
+    if (empty($store_record) && !empty($domain)) {
+        $store_record = $db_engine->query("SELECT * FROM sys_websites WHERE domain = ? LIMIT 1", [$domain]);
+    }
+
+    
+    $store_id = $store_record[0]['id'] ?? '';
+    return $store_id; 
+}
+
+echo _get_store_id("secrets.state");
+
 ?>
