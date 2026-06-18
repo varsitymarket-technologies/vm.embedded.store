1a5530377ab34b7dc12ad791c081f0e933ea0c6a
levidoc <levidoc00@gmail.com>
2026-06-18 23:59:22 +0200

    Vetting Update

--- sys.update.sh
+++ sys.update.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#   TITLE   : Application System Update Script   
+#   DESC    : This script updates the application to the latest version from the master branch of the git repository. 
+#   PROPRIETOR: VARSITYMARKET_TECHNOLOGIES
+#   VERSION : 1.0.1.1
+#   AUTHOR  : HARDY HASTINGS  
+#   RELEASE : 2026/06/18
+
 git fetch origin master 
 git reset --hard origin/master
 #cp copy.env .env
